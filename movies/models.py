3a0ffead8c9223a50911adc4f49330e0fd764a9c
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-29 00:06:10 +0300

    birthplace and carrier role for actors

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,20 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarrierRole(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Countries(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
 class Actor(models.Model):
     MALE = 'M'
     FEMALE = 'F'
@@ -17,9 +31,13 @@ class Actor(models.Model):
     gender = models.CharField(max_length=1, choices=GENDERS, default=MALE)
     slug = models.SlugField(default='', null=False, db_index=True)
     birthday = models.DateField(blank=True, null=True)
+    carrier_role = models.ManyToManyField(CarrierRole, blank=True, default="No information")
+    birthplace = models.CharField(max_length=150, default="No information")
 
     def get_url(self):
         return reverse('actor_detail', args=[self.slug])
+
+    # def _get_total_films(self):
 
     def __str__(self):
         if self.gender == self.MALE:
@@ -53,13 +71,6 @@ class Director(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-class Countries(models.Model):
-    name = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     director = models.ForeignKey(Director, on_delete=models.PROTECT, null=True, blank=True, related_name='movies')
@@ -70,7 +81,7 @@ class Movie(models.Model):
                                                  null=True)
     rating_IMBd = models.DecimalField(max_digits=3, decimal_places=1,
                                       validators=[MaxValueValidator(10), MinValueValidator(0)], blank=True, null=True)
-    countries = models.ManyToManyField(Countries)
+    countries = models.ManyToManyField(Countries, blank=True, default="")
     timing = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0)])
     date = models.DateField(blank=True, null=True)
     year = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1960), MaxValueValidator(2030)])
