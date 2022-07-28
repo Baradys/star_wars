4a01f15983fdec4b85c80b32805eb342b1eadc17
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-28 22:40:02 +0300

    movie detail added

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MoviesView.as_view()),
+    path('<slug>', views.MovieDetail.as_view(), name='movie_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,9 @@ class MoviesView(ListView):
     template_name = 'movies/all_movies.html'
     model = Movie
     context_object_name = 'movies'
+
+
+class MovieDetail(DetailView):
+    template_name = 'movies/one_movie.html'
+    model = Movie
+    context_object_name = 'movie'
