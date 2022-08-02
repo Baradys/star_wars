ecf6d140886100b50f4c417f6d83f0ac5fe80946
Mikhail Bratukhin <bym97m@mail.ru>
2022-08-02 11:46:58 +0300

    posters have been added to admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import Movie, Countries, Composer, Director, Actor
+from django.utils.safestring import mark_safe
 
 
 # Register your models here.
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'year', 'timing', 'rating_rotten_tomatoes', 'rating_IMBd']
+    list_display = ['name', 'poster_show', 'year', 'timing', 'rating_rotten_tomatoes', 'rating_IMBd']
     filter_horizontal = ['countries', 'actors']
     prepopulated_fields = {'slug': ('name',)}
+
+    def poster_show(self, poster):
+        if poster.movie_poster:
+            return mark_safe(f"<img src='{poster.movie_poster.url}' width='60'")
+        return None
+
+    poster_show.__name__ = 'poster'
 
 
 @admin.register(Countries)
