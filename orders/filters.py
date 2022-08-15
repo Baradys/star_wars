ff528ba8c83f0766f6a10dd481f3156520ff6ed5
Mikhail Bratukhin <bym97m@mail.ru>
2022-08-15 21:47:00 +0300

    switching from SQLite3 to PostgreSQL

--- orders/filters.py
+++ orders/filters.py
@@ -3,8 +3,7 @@ from .models import Order
 
 
 class OrderFilter(django_filters.FilterSet):
-    # years = Order.created.all
-    # print(years)
+    # years = Order.objects.distinct('created')
     CHOICES = list((i, i) for i in range(2019, 2023))
     release_year = django_filters.ChoiceFilter(field_name='created', lookup_expr='year', choices=CHOICES)
 
